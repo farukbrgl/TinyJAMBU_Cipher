869f1d6ea1804271400374a497e0593319b0c172
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-01-17 14:46:03 +0300

    Initial commit

---
+++ python/tinyjambu.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+TinyJAMBU Hafif Blok Şifreleyicisinin Python ile Gerçeklenmesi
+Orijinal makaleye
+https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/tinyjambu-spec-final.pdf
+adresinden ulaşabilirsiniz.
+"""
