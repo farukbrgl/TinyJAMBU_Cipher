1061a953e9a6001ad0b42e860e869582aa64a83e
Ömer Faruk Birgül <farukbrgl@protonmail.com>
2022-02-14 00:12:19 +0300

    update tiny jambu

--- python/tinyjambu.py
+++ python/tinyjambu.py
@@ -1,9 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
-
+import tinyjambu_perm as perm
 """
 TinyJAMBU Hafif Blok Şifreleyicisinin Python ile Gerçeklenmesi
 Orijinal makaleye
 https://csrc.nist.gov/CSRC/media/Projects/lightweight-cryptography/documents/finalist-round/updated-spec-doc/tinyjambu-spec-final.pdf
 adresinden ulaşabilirsiniz.
 """
+
+
+klen = 128
+S = [0x0]*128
+K = [0x0]*128
+N = [0x0]*96
+T = [0x0]*64
+FB_n = [0,0,1]
+FB_ad= [0,1,1]
+FB_pc= [1,0,1]
+FB_f = [1,1,1]
+
+## The initialization
+# Key setup
+S = perm.state_update(S, K, 1024)
+
+# Nonce setup
+for i in range (2):
+    a = 0
+    b = 0
+    S[36:38] = list(a ^ b for a, b in zip(S[36:38], FB_n))
+    S = perm.state_update(S, K, 640)
+    S[96:127] = list(a ^ b for a, b in zip(S[96:127], N[32*i:32*i+31]))
+
+
+## Processing the associated data
+
+print(S)
---
+++ python/tinyjambu_perm.py
@@ -0,0 +1,19 @@
+def state_update(S, K, i_round):
+    for i in range (i_round):
+        feedback = S[0] ^ S[47] ^ (0x1 - (S[70] & S[85])) ^ S[91] ^ (K[(i % klen)])
+        for j in range(127):
+            S.insert(0, S.pop())
+            # print(S)
+        S[127] = feedback
+    return S
+
+#128-bit key
+#96-bit nonce
+#64-bit tag
+#128-bit state
+klen = 128
+S = [0x0]*128
+K = [0x0]*128
+
+a = state_update(S, K, 640)
+print(a)
